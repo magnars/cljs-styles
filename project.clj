8f28b257c6e69061313164ef8a45d30d76fbf226
Magnar Sveen <magnars@gmail.com>
2017-06-23 08:50:21 +0200

    Release 0.3.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-styles "0.3.6"
+(defproject cljs-styles "0.3.7"
   :description "Vendor prefixes for React inline styles with ClojureScript"
   :url "https://github.com/magnars/cljs-styles"
   :license {:name "Eclipse Public License"
