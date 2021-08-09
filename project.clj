3173fbd6f8e6cea64ceb838e43b50a2c1a9b5f4b
Magnar Sveen <magnars@gmail.com>
2021-08-09 09:28:07 +0200

    Release 0.3.9

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
-(defproject cljs-styles "0.3.8"
+(defproject cljs-styles "0.3.9"
   :description "Vendor prefixes for React inline styles with ClojureScript"
   :url "https://github.com/magnars/cljs-styles"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies []
   :profiles {:dev {:plugins [[lein-expectations "0.0.7"]
                              [lein-autoexpect "1.6.0"]]
-                   :dependencies [[expectations "2.1.4"]
+                   :dependencies [[org.clojure/clojure "1.8.0"]
+                                  [expectations "2.1.4"]
                                   [flare "0.2.9"]]
                    :injections [(require 'flare.expectations)
                                 (flare.expectations/install!)]}})
